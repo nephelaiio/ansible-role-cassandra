200bad77f71d7f9b5f709e863070a2bd758f8598
Ted Cook <teodoro.cook@gmail.com>
2020-10-22 17:07:12 -0600

    Add role management tasks

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,19 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+def cqlsh(u, p, c):
+    return f"cqlsh -u {u} -p {p} -e '{c}'"
+
+
 def hostname(host):
     return host.ansible.get_variables()['inventory_hostname']
 
 
 def test_status(host):
     assert host.command('nodetool status').rc == 0
+
+
+def test_roles(host):
+    assert host.command(cqlsh('cassandra', 'cassandra', 'quit;')).rc == 0
+    assert host.command(cqlsh('topsecret', 'topsecret', 'quit;')).rc == 0
+    assert host.command(cqlsh('nosecret', 'nosecret', 'quit;')).rc == 0
